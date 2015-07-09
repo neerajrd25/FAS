fd64540f499f3e29fc28f1e1423bbadceba2111d
neerajrd25 <kumaratneeraj@gmail.com>
2015-07-09 22:58:36 +0530

    First Commit

---
+++ FlairAutomationSolutions/WebContent/flair/aboutus.php
@@ -0,0 +1,260 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Team - Flair Automation Solution</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta name="description" content="" />
+<meta name="author" content="http://bootstraptaste.com" />
+<!-- css -->
+<link href="css/bootstrap.min.css" rel="stylesheet" />
+<link href="css/fancybox/jquery.fancybox.css" rel="stylesheet">
+<link href="css/jcarousel.css" rel="stylesheet" />
+<link href="css/flexslider.css" rel="stylesheet" />
+<link href="css/style.css" rel="stylesheet" />
+<link href="css/custom.css" rel="stylesheet" />
+
+
+
+
+
+<!-- Theme skin -->
+<link href="skins/default.css" rel="stylesheet" />
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:700' rel='stylesheet' type='text/css' />
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:600' rel='stylesheet' type='text/css' />
+<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+<!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+</head>
+<body class="aboutus">
+<?php require 'header.html';?>
+
+<div id="wrapper">
+
+	<!-- start header -->
+	<!-- <header>
+        <div class="navbar navbar-default navbar-static-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="index.html"><span class="firstLogoWord" style="color:#FF6600;">flair</span> <span class="SecondLogoWord" style="color:#0028CA;">Automation Solutions</span></a>
+                </div>
+                <div class="navbar-collapse collapse ">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="index.html">Home</a></li>
+                        <li class="active"><a href="aboutus.html">About us</a></li>
+                        
+                       	<li style="padding-bottom: 0px;" id="multimenu">
+                       	<a href="#" class="dropdown-toggle" data-toggle="dropdown">Products</a>
+                    		<ul class="dropdown-menu multi-level">
+                    			<li class="dropdown-submenu">
+		                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Mechanical Design</a>
+		                            <ul class="dropdown-menu">
+		                                <li><a href="#">Jigs &#038; fixtures design</a></li>
+		                                <li><a href="#">Process Structural Design</a></li>
+		                            </ul>
+		                        </li>
+
+                    			<li class="dropdown-submenu">
+		                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Installations &#38; Integration</a>
+		                            <ul class="dropdown-menu">
+		                                <li><a href="#">Mechanical Assembly</a></li>
+		                                <li><a href="#">Mechanical Installations</a></li>
+		                                <li><a href="#">Controls Integrations</a></li>
+		                                <li><a href="#">Controls Installations</a></li>
+		                            </ul>
+		                        </li>
+
+		                        <li class="dropdown-submenu">
+		                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Controls Systems</a>
+		                            <ul class="dropdown-menu">
+		                                <li><a href="#">Concept &#38; System Design</a></li>
+		                                <li><a href="#">Panel Design</a></li>
+		                            </ul>
+		                        </li>
+
+		                        <li class="dropdown-submenu">
+		                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Machinery &#38; Relocation</a>
+		                            <ul class="dropdown-menu">
+		                                <li><a href="#">-NA-</a></li>
+		                                <li><a href="#">-NA-</a></li>
+		                            </ul>
+		                        </li>
+             
+                    		</ul>
+
+                		</li>
+             
+
+                        <li><a href="gallery.html">Gallery</a></li>                        
+                        <li><a href="team.html">Team</a></li>
+                        <li><a href="contact.php">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+	</header> -->
+	<!-- end header -->
+	<section id="inner-headline">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<ul class="breadcrumb">
+						<li><a href="#"><i class="fa fa-home"></i></a><i class="icon-angle-right"></i></li>
+						<li class="active">About us</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</section>
+<!-- start about us -->
+	<h2 class="centered" align="center">Mission and Vision</h2>
+	<section id="cd-timeline" class="cd-container">
+		<div class="cd-timeline-block">
+			<div class="cd-timeline-img cd-picture">
+				<img src="img/avatar.png" alt="Picture">
+			</div> <!-- cd-timeline-img -->
+
+			<div class="cd-timeline-content">
+				<h2 class="aboutushead2">Inception</h2>
+				<p>Flair Automation Solutions is a Pune based organization started in May 2014. We provide solutions for automotive body welding lines with expertise in Process, Mechanical &#38; Controls Design, Assembly, Integration, Installation and Project Management. <br/><br/>We, at flair, intend to grow as a integrator for automotive body shops by providing world class solutions.</p>
+				<a href="#0" class="cd-read-more;" style="visibility:hidden;">Read more</a>
+				<span class="cd-date">Inception</span>
+			</div> <!-- cd-timeline-content -->
+		</div> <!-- cd-timeline-block -->
+
+		<div class="cd-timeline-block">
+			<div class="cd-timeline-img cd-movie">
+				<img src="img/avatar.png" alt="Movie">
+			</div> <!-- cd-timeline-img -->
+
+			<div class="cd-timeline-content">
+				<h2 class="aboutushead2">Growth</h2>
+				<p>
+					<ul class="list-group">
+					  <li class="list-group-item">May 2014 : Started with consultancy.</li>
+					  <li class="list-group-item">December 2014 : Develop Controls Integration Team.</li>
+					  <li class="list-group-item"> February 2015 :  Develop Mechanical &#38; Controls Design Team.</li>
+					  <li class="list-group-item">March 2015 :  Develop Mechanical Assembly &#38; Installation Team.</li>
+					</ul>
+				</p>
+				<a href="#0" class="cd-read-more;" style="visibility:hidden;">Read more</a>
+				<span class="cd-date">Growth</span>
+			</div> <!-- cd-timeline-content -->
+		</div> <!-- cd-timeline-block -->
+
+		<div class="cd-timeline-block">
+			<div class="cd-timeline-img cd-picture">
+				<img src="img/avatar.png" alt="Movie">
+			</div> <!-- cd-timeline-img -->
+
+			<div class="cd-timeline-content">
+				<h2 class="aboutushead2">Now</h2>
+				<p> From Consulting &#38; Servicing  we are already moved toward Project Proposal, Design, Assembly &#38; Installations.<br/><br/>We keep delivering the products successfully on time.</p>
+				<a href="#0" class="cd-read-more;" style="visibility:hidden;">Read more</a>
+				<span class="cd-date">Now</span>
+			</div> <!-- cd-timeline-content -->
+		</div> <!-- cd-timeline-block -->
+	</section>
+
+		<!-- end about us -->
+	<footer>
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-3">
+					<div class="widget">
+						<h5 class="widgetheading">Get in touch with us</h5>
+						<address>
+						<strong>flair Automation Solutions</strong><br/>
+						 S. NO - 22/B-1,  Balajinagar,<br/>
+						 Pune-Satara road, Pune-411043</address>
+						
+					</div>
+				</div>
+				<div class="col-lg-3">
+				<div class="widget">
+					<p>
+						<h5 class="widgetheading">Contact Details</h5>
+						<i class="icon-phone"></i>+91 8888891627<br/>
+						<i class="icon-envelope-alt"></i>sb.flair@gmail.com<br/><br/>
+						<i class="icon-phone"></i>+91 7218720004<br/>
+						<i class="icon-envelope-alt"></i> nikhilesh.patel@flairautomation.com<br/><br/>
+						<i class="icon-phone"></i>+91 7722033607<br/>
+						<i class="icon-envelope-alt"></i> abhijeet.n@flairautomation.com<br/>
+					</p>
+				</div>
+			</div>
+				<div class="col-lg-3">
+					<div class="widget">
+						<h5 class="widgetheading">Pages</h5>
+						<ul class="link-list">
+							<li><a href="#">Products</a></li>
+							<li><a href="#">Terms and conditions</a></li>
+							<li><a href="#">Privacy policy</a></li>
+							<li><a href="#">Career center</a></li>
+							<li><a href="#">Contact us</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-lg-3" style="display:none;">
+					<div class="widget">
+						<h5 class="widgetheading">Latest posts</h5>
+						<ul class="link-list">
+							<li><a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a></li>
+							<li><a href="#">Pellentesque et pulvinar enim. Quisque at tempor ligula</a></li>
+							<li><a href="#">Natus error sit voluptatem accusantium doloremque</a></li>
+						</ul>
+					</div>
+				</div>
+				
+		</div>
+	<div id="sub-footer">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6">
+					<div class="copyright">
+						<p>
+							<span>&copy; 2015 flair Automation Solutions  All right reserved.
+						</p>
+					</div>
+				</div>
+				<div class="col-lg-6">
+					<ul class="social-network">
+						<li><a href="#" data-placement="top" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+						<li><a href="#" data-placement="top" title="Twitter"><i class="fa fa-twitter"></i></a></li>
+						<li><a href="#" data-placement="top" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
+						<li><a href="#" data-placement="top" title="Pinterest"><i class="fa fa-pinterest"></i></a></li>
+						<li><a href="#" data-placement="top" title="Google plus"><i class="fa fa-google-plus"></i></a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+	</footer>
+	</div>
+	<a href="#" class="scrollup"><i class="fa fa-angle-up active"></i></a>
+<!-- javascript
+    ================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="js/jquery.js"></script>
+<script src="js/jquery.easing.1.3.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.fancybox.pack.js"></script>
+<script src="js/jquery.fancybox-media.js"></script>
+<script src="js/google-code-prettify/prettify.js"></script>
+<script src="js/portfolio/jquery.quicksand.js"></script>
+<script src="js/portfolio/setting.js"></script>
+<script src="js/jquery.flexslider.js"></script>
+<script src="js/animate.js"></script>
+<script src="js/custom.js"></script>
+<script src="js/aboutus/modernizr.js"></script> <!-- Modernizr -->
+<script src="js/aboutus/main.js"></script> <!-- Resource jQuery -->
+
+</body>
+</html>
