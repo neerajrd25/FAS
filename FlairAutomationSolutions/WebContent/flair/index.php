b9a7491f3aa1f3abe9b668b28f9ab49a72a0ff83
neerajrd25 <kumaratneeraj@gmail.com>
2015-07-13 20:56:18 +0530

    php

---
+++ FlairAutomationSolutions/WebContent/flair/index.php
@@ -0,0 +1,276 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Flair Automation Solutions</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta name="description" content="" />
+<meta name="author" content="" />
+<!-- css -->
+<link href="css/bootstrap.min.css" rel="stylesheet" />
+<link href="css/fancybox/jquery.fancybox.css" rel="stylesheet">
+<link href="css/jcarousel.css" rel="stylesheet" />
+<link href="css/flexslider.css" rel="stylesheet" />
+<link href="css/style.css" rel="stylesheet" />
+<link href="css/custom.css" rel="stylesheet" />
+
+<!-- Theme skin -->
+<link href="skins/default.css" rel="stylesheet" />
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:700' rel='stylesheet' type='text/css' />
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:600' rel='stylesheet' type='text/css' />
+
+<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+<!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+</head>
+<body>
+<div id="wrapper" >
+	<!-- start header -->
+        <?php include("header.html");?>
+	<!-- end header -->
+
+	<!-- start container -->
+
+		<section id="featured">
+			<!-- start slider -->
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12">
+			<!-- Slider -->
+			    <div id="main-slider" class="flexslider">
+			        <ul class="slides">
+			          <li>
+			            <img src="img/slides/mechanicaldesign.jpg" width="600" alt="" />
+			            <div class="flex-caption">
+			                <h3>Mechanical Design</h3> 
+							<p></p> 
+							<a href="#" class="btn btn-theme">Learn More</a>
+			            </div>
+			          </li>
+			          <li>
+			            <img src="img/slides/equipmentdesign.jpg" width="600" alt="" />
+			            <div class="flex-caption">
+			                <h3>EQUIPMENT DESIGN &#38; LAYOUT</h3> 
+							<p></p> 
+							<a href="#" class="btn btn-theme">Learn More</a>
+			            </div>
+			          </li>
+			          <li>
+			            <img src="img/slides/control.jpg" width="600" alt="" />
+			            <div class="flex-caption">
+			                <h3>CONTROLS &#38; AUTOMATION</h3> 
+							<p></p> 
+							<a href="#" class="btn btn-theme">Learn More</a>
+			            </div>
+			          </li>
+			          <li>
+			            <img src="img/slides/manufacturing.jpg" width="600" alt="" />
+			            <div class="flex-caption">
+			                <h3>MANUFACTURING &#38; SITE  MANAGEMENT</h3> 
+							<p></p> 
+							<a href="#" class="btn btn-theme">Learn More</a>
+			            </div>
+			          </li>
+			          
+			        </ul>
+			    </div>
+			<!-- end slider -->
+					</div>
+				</div>
+			</div>	
+		</section>
+		<section class="callaction">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12">
+						<div class="big-cta">
+							<div class="cta-text" style="display:none;">
+								<h2>Our Products</h2>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section id="content">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12">
+						<div class="row">
+							<div class="col-lg-3">
+								<div class="box">
+									<div class="box-gray aligncenter" style="padding-bottom: 64px;">
+										<h4>MECHANICAL DESIGN &#38; PROCESS</h4>
+										<div class="icon">
+										<i class="fa fa-desktop fa-3x"></i>
+										</div>
+										<p class="list_style">
+											<i class="fa fa-chevron-right"></i>&nbsp;Proposal<br/>
+											<i class="fa fa-chevron-right"></i>&nbsp;Process<br/>
+											<i class="fa fa-chevron-right"></i>&nbsp;Layout<br/>
+											<i class="fa fa-chevron-right"></i>&nbsp;Mechanical Design
+										</p>									
+									</div>
+									<div class="box-bottom">
+										<a href="#">Learn more</a>
+									</div>
+								</div>
+							</div>
+							<div class="col-lg-3">
+								<div class="box">
+									<div class="box-gray aligncenter" style="padding-bottom: 26px;"> 
+										<h4>EQUIPMENT DESIGN &#38; LAYOUT</h4>
+										<div class="icon">
+										<i class="fa fa-pagelines fa-3x"></i>
+										</div>
+										<p class="list_style">
+											<i class="fa fa-chevron-right"></i>&nbsp;Proposal<br/>
+											<i class="fa fa-chevron-right"></i>&nbsp;Process<br/>
+											<i class="fa fa-chevron-right"></i>&nbsp;Layout<br/>
+											<i class="fa fa-chevron-right"></i>&nbsp;Equipment Design<br/>
+											<i class="fa fa-chevron-right"></i>&nbsp;Transfer Systems
+										</p>									
+									</div>
+									<div class="box-bottom">
+										<a href="#">Learn more</a>
+									</div>
+								</div>
+							</div>
+							<div class="col-lg-3">
+								<div class="box">
+									<div class="box-gray aligncenter" style="padding-bottom: 26px;">
+										<h4>CONTROLS &#38; AUTOMATION</h4>
+										<div class="icon">
+										<i class="fa fa-edit fa-3x"></i>
+										</div>
+										<p class="list_style">
+											<i class="fa fa-chevron-right"></i>&nbsp;Process<br/>
+											<i class="fa fa-chevron-right"></i>&nbsp;Controls Design<br/>
+											<span class="specail_list"><i class="fa fa-chevron-right"></i>&nbsp;Integration of Robotic &#38; Controls Systems</span><br/>
+											<i class="fa fa-chevron-right"></i>&nbsp;Installation<br/>
+											<i class="fa fa-chevron-right"></i>&nbsp;PLC Programming
+										</p>									
+									</div>
+									<div class="box-bottom">
+										<a href="#">Learn more</a>
+									</div>
+								</div>
+							</div>
+							<div class="col-lg-3">
+								<div class="box">
+									<div class="box-gray aligncenter" style="padding-bottom: 26px;">
+										<h4>MANUFACTURING &#38; SITE MANAGEMENT</h4>
+										<div class="icon">
+										<i class="fa fa-code fa-3x"></i>
+										</div>
+										<p class="list_style">
+											<i class="fa fa-chevron-right"></i>&nbsp;Manufacturing<br/>
+											<i class="fa fa-chevron-right"></i>&nbsp;Assembly<br/>
+											<i class="fa fa-chevron-right"></i>&nbsp;Installation<br/>
+											<i class="fa fa-chevron-right"></i>&nbsp;Tryouts<br/>
+											<i class="fa fa-chevron-right"></i>&nbsp;Buy Off
+										</p>
+											
+									</div>
+									<div class="box-bottom">
+										<a href="#">Learn more</a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- divider -->
+				<div class="row">
+					<div class="col-lg-12">
+						<div class="solidline">
+						</div>
+					</div>
+				</div>
+				<!-- end divider -->
+				<!-- Portfolio Projects -->
+				<div class="row" style="display:none;">
+					<div class="col-lg-12">
+						<h4 class="heading">Recent Works</h4>
+						<div class="row">
+							<section id="projects">
+							<ul id="thumbs" class="portfolio">
+								<!-- Item Project and Filter Name -->
+								<li class="col-lg-3 design" data-id="id-0" data-type="web">
+								<div class="item-thumbs">
+								<!-- Fancybox - Gallery Enabled - Title - Full Image -->
+								<a class="hover-wrap fancybox" data-fancybox-group="gallery" title="Work 1" href="img/works/1.jpg">
+								<span class="overlay-img"></span>
+								<span class="overlay-img-thumb font-icon-plus"></span>
+								</a>
+								<!-- Thumb Image and Description -->
+								<img src="img/works/1.jpg" alt="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis elementum odio. Curabitur pellentesque, dolor vel pharetra mollis.">
+								</div>
+								</li>
+								<!-- End Item Project -->
+								<!-- Item Project and Filter Name -->
+								<li class="item-thumbs col-lg-3 design" data-id="id-1" data-type="icon">
+								<!-- Fancybox - Gallery Enabled - Title - Full Image -->
+								<a class="hover-wrap fancybox" data-fancybox-group="gallery" title="Work 2" href="img/works/2.jpg">
+								<span class="overlay-img"></span>
+								<span class="overlay-img-thumb font-icon-plus"></span>
+								</a>
+								<!-- Thumb Image and Description -->
+								<img src="img/works/2.jpg" alt="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis elementum odio. Curabitur pellentesque, dolor vel pharetra mollis.">
+								</li>
+								<!-- End Item Project -->
+								<!-- Item Project and Filter Name -->
+								<li class="item-thumbs col-lg-3 photography" data-id="id-2" data-type="illustrator">
+								<!-- Fancybox - Gallery Enabled - Title - Full Image -->
+								<a class="hover-wrap fancybox" data-fancybox-group="gallery" title="Work 3" href="img/works/3.jpg">
+								<span class="overlay-img"></span>
+								<span class="overlay-img-thumb font-icon-plus"></span>
+								</a>
+								<!-- Thumb Image and Description -->
+								<img src="img/works/3.jpg" alt="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis elementum odio. Curabitur pellentesque, dolor vel pharetra mollis.">
+								</li>
+								<!-- End Item Project -->
+								<!-- Item Project and Filter Name -->
+								<li class="item-thumbs col-lg-3 photography" data-id="id-2" data-type="illustrator">
+								<!-- Fancybox - Gallery Enabled - Title - Full Image -->
+								<a class="hover-wrap fancybox" data-fancybox-group="gallery" title="Work 4" href="img/works/4.jpg">
+								<span class="overlay-img"></span>
+								<span class="overlay-img-thumb font-icon-plus"></span>
+								</a>
+								<!-- Thumb Image and Description -->
+								<img src="img/works/4.jpg" alt="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis elementum odio. Curabitur pellentesque, dolor vel pharetra mollis.">
+								</li>
+								<!-- End Item Project -->
+							</ul>
+							</section>
+						</div>
+					</div>
+				</div>
+
+			</div>
+		</section>
+	<!-- end container -->
+
+	<!-- start footer -->
+        <?php include("footer.html");?>
+	<!-- end footer -->
+</div>
+<a href="#" class="scrollup"><i class="fa fa-angle-up active"></i></a>
+<!-- javascript
+    ================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="js/jquery.js"></script>
+<script src="js/jquery.easing.1.3.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.fancybox.pack.js"></script>
+<script src="js/jquery.fancybox-media.js"></script>
+<script src="js/google-code-prettify/prettify.js"></script>
+<script src="js/portfolio/jquery.quicksand.js"></script>
+<script src="js/portfolio/setting.js"></script>
+<script src="js/jquery.flexslider.js"></script>
+<script src="js/animate.js"></script>
+<script src="js/custom.js"></script>
+</body>
+</html>
---
+++ FlairAutomationSolutions/WebContent/flair/team.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Team - Flair Automation Solution</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta name="description" content="" />
+<meta name="author" content="http://bootstraptaste.com" />
+<!-- css -->
+<link href="css/bootstrap.min.css" rel="stylesheet" />
+<link href="css/fancybox/jquery.fancybox.css" rel="stylesheet">
+<link href="css/jcarousel.css" rel="stylesheet" />
+<link href="css/flexslider.css" rel="stylesheet" />
+<link href="css/style.css" rel="stylesheet" />
+<link href="css/custom.css" rel="stylesheet" />
+
+
+
+<!-- Theme skin -->
+<link href="skins/default.css" rel="stylesheet" />
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:700' rel='stylesheet' type='text/css' />
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:600' rel='stylesheet' type='text/css' />
+
+<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+<!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+</head>
+<body>
+<div id="wrapper">
+
+	<!-- start header -->
+        <?php include("header.html");?>
+	<!-- end header -->
+
+	<section id="inner-headline">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<ul class="breadcrumb">
+						<li><a href="#"><i class="fa fa-home"></i></a><i class="icon-angle-right"></i></li>
+						<li class="active">Team</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- start team -->
+		<section id="container">
+			<div class="container" id="team" name="team">
+					<br>
+						<div class="row white centered">
+							<h1 class="centered" align="center">MEET OUR AWESOME TEAM</h1>
+							<hr>
+							<br>
+							<br>
+							<div class="col-lg-3 centered">
+								<img class="img img-circle" src="img/team/person.svg" height="120px" width="120px" alt="">
+								<br>
+								<h4><b>Mr. Mayur Valte</b>&nbsp;<a href="#" class="fa fa-twitter" style="font-size:smaller;"></a>&nbsp;<a href="#" class="fa fa-facebook" style="font-size:smaller;"></a><br/><em style="font-size:smaller;">Mech-Design</em></h4>
+								
+								<p style="display:none">Description will come here</p>
+							</div><!-- col-lg-3 -->
+							
+							<div class="col-lg-3 centered">
+								<img class="img img-circle" src="img/team/person.svg" height="120px" width="120px" alt="">
+								<br>
+								<h4><b>Mr. Aaditya Kashid</b>&nbsp;<a href="#" class="fa fa-twitter" style="font-size:smaller;"></a>&nbsp;<a href="#" class="fa fa-facebook" style="font-size:smaller;"></a><br/><em style="font-size:smaller;">Mech-Design</em></h4>
+								
+								<p style="display:none">Description will come here</p>
+							</div><!-- col-lg-3 -->
+							
+							<div class="col-lg-3 centered">
+								<img class="img img-circle" src="img/team/person.svg" height="120px" width="120px" alt="">
+								<br>
+								<h4><b>Mr. Abhijeet Nimbalkar</b>&nbsp;<a href="#" class="fa fa-twitter" style="font-size:smaller;"></a>&nbsp;<a href="#" class="fa fa-facebook" style="font-size:smaller;"></a><br/><em style="font-size:smaller;">Head Engineering</em></h4>
+								
+								<p style="display:none">Description will come here</p>
+							</div><!-- col-lg-3 -->
+							
+							<div class="col-lg-3 centered">
+								<img class="img img-circle" src="img/team/person.svg" height="120px" width="120px" alt="">
+								<br>
+								<h4><b>Mr. Nikhilesh Patel</b>&nbsp;<a href="#" class="fa fa-twitter" style="font-size:smaller;"></a>&nbsp;<a href="#" class="fa fa-facebook" style="font-size:smaller;"></a><br/><em style="font-size:smaller;">Head Manufacturing</em></h4>
+								
+								<p style="display:none">Description will come here</p>
+							</div><!-- col-lg-3 -->
+							
+						</div><!-- row -->
+						<div class="row white centered" >
+						<div class="col-lg-3 centered" >
+								<img class="img img-circle" src="img/team/person.svg" height="120px" width="120px" alt="">
+								<br>
+								<h4><b>Mr. Sandip Bhosale</b>&nbsp;<a href="#" class="fa fa-twitter" style="font-size:smaller;"></a>&nbsp;<a href="#" class="fa fa-facebook" style="font-size:smaller;"></a><br/><em style="font-size:smaller;">Head-Controls &#38; Automation</em></h4>
+								
+								<p style="display:none">Description will come here</p>
+							</div><!-- col-lg-3 -->
+						</div>
+			</div>
+		</section>
+	<!-- end team -->
+
+	<!-- start footer -->
+        <?php include("footer.html");?>
+	<!-- end footer -->
+</div>
+<a href="#" class="scrollup"><i class="fa fa-angle-up active"></i></a>
+<!-- javascript
+    ================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="js/jquery.js"></script>
+<script src="js/jquery.easing.1.3.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.fancybox.pack.js"></script>
+<script src="js/jquery.fancybox-media.js"></script>
+<script src="js/google-code-prettify/prettify.js"></script>
+<script src="js/portfolio/jquery.quicksand.js"></script>
+<script src="js/portfolio/setting.js"></script>
+<script src="js/jquery.flexslider.js"></script>
+<script src="js/animate.js"></script>
+<script src="js/custom.js"></script>
+</body>
+</html>
