8db1be544ec08cfe68cbccb773c85595e5ff255c
neerajrd25 <kumaratneeraj@gmail.com>
2015-07-21 22:44:09 +0530

    Title Changd

--- aboutus.php
+++ aboutus.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 <meta charset="utf-8">
-<title>Team - Flair Automation Solution</title>
+<title>Flair Automation Solution</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <meta name="description" content="" />
 <meta name="author" content="http://bootstraptaste.com" />
