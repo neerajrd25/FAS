fcb8850493081cd2fbe406e20cd6c467e9312977
Neeraj <kumaratneeraj@gmail.com>
2015-07-30 00:43:18 +0530

    box alignment is done that you send

--- index.php
+++ index.php
@@ -11,68 +11,55 @@
 <!-- end common header -->
 </head>
 <body>
-<div id="wrapper" >
-	<!-- start header -->
+	<div id="wrapper">
+		<!-- start header -->
         <?php include("header.html");?>
 	<!-- end header -->
 
-	<!-- start container -->
+		<!-- start container -->
 
 		<section id="featured">
 			<!-- start slider -->
 			<div class="container">
 				<div class="row">
 					<div class="col-lg-12">
-			<!-- Slider -->
-			    <div id="main-slider" class="flexslider">
-			        <ul class="slides">
-			          <li>
-			            <img src="img/slides/mechanicaldesign.jpg" width="600" alt="" />
-			            <div class="flex-caption">
-			                <h3>Mechanical Design</h3> 
-							<p></p> 
-							<a href="#" class="btn btn-theme">Learn More</a>
-			            </div>
-			          </li>
-			          <li>
-			            <img src="img/slides/equipmentdesign.jpg" width="600" alt="" />
-			            <div class="flex-caption">
-			                <h3>EQUIPMENT DESIGN &#38; LAYOUT</h3> 
-							<p></p> 
-							<a href="#" class="btn btn-theme">Learn More</a>
-			            </div>
-			          </li>
-			          <li>
-			            <img src="img/slides/control.jpg" width="600" alt="" />
-			            <div class="flex-caption">
-			                <h3>CONTROLS &#38; AUTOMATION</h3> 
-							<p></p> 
-							<a href="#" class="btn btn-theme">Learn More</a>
-			            </div>
-			          </li>
-			          <li>
-			            <img src="img/slides/manufacturing.jpg" width="600" alt="" />
-			            <div class="flex-caption">
-			                <h3>MANUFACTURING &#38; SITE  MANAGEMENT</h3> 
-							<p></p> 
-							<a href="#" class="btn btn-theme">Learn More</a>
-			            </div>
-			          </li>
-			          
-			        </ul>
-			    </div>
-			<!-- end slider -->
+						<!-- Slider -->
+						<div id="main-slider" class="flexslider">
+							<ul class="slides">
+								<li><img src="img/slides/mechanicaldesign.jpg" width="600" alt="" />
+									<div class="flex-caption">
+										<h3>Mechanical Design</h3>
+										<p></p>
+										<a href="#" class="btn btn-theme">Learn More</a>
+									</div></li>
+
+								<li><img src="img/slides/control.jpg" width="600" alt="" />
+									<div class="flex-caption">
+										<h3>CONTROLS &#38; AUTOMATION</h3>
+										<p></p>
+										<a href="#" class="btn btn-theme">Learn More</a>
+									</div></li>
+								<li><img src="img/slides/manufacturing.jpg" width="600" alt="" />
+									<div class="flex-caption">
+										<h3>MANUFACTURING &#38; SITE MANAGEMENT</h3>
+										<p></p>
+										<a href="#" class="btn btn-theme">Learn More</a>
+									</div></li>
+
+							</ul>
+						</div>
+						<!-- end slider -->
 					</div>
 				</div>
-			</div>	
+			</div>
 		</section>
 		<section class="callaction">
 			<div class="container">
 				<div class="row">
 					<div class="col-lg-12">
 						<div class="big-cta">
-							<div class="cta-text" style="display:none;">
-								<h2>Our Products</h2>
+							<div class="cta-text">
+								<h2>Our Services</h2>
 							</div>
 						</div>
 					</div>
@@ -84,71 +71,70 @@
 				<div class="row">
 					<div class="col-lg-12">
 						<div class="row">
-							<div class="col-lg-2">
-							</div>
-							<div class="col-lg-3">
-								<div class="box">
-									<div class="box-gray aligncenter" style="padding-bottom: 40px;">
+
+							<div class="col-lg-4">
+								<div class="box" style="width: 100%">
+									<div class="box-gray aligncenter" style="padding-bottom: 38px;">
 										<h4 style="margin-bottom: 39px;">MECHANICAL DESIGN</h4>
 										<div class="icon">
-										<i class="fa fa-desktop fa-3x"></i>
+											<i class="fa fa-picture-o fa-3x"></i>
 										</div>
 										<p class="list_style">
-											<i class="fa fa-chevron-right"></i>&nbsp;Proposal<br/>
-											<i class="fa fa-chevron-right"></i>&nbsp;Process<br/>
-											<i class="fa fa-chevron-right"></i>&nbsp;Layout<br/>
-											<i class="fa fa-chevron-right"></i>&nbsp;Design
-										</p>									
+											<i class="fa fa-chevron-right"></i>&nbsp;Proposal<br /> <i
+												class="fa fa-chevron-right"></i>&nbsp;Process<br /> <i
+												class="fa fa-chevron-right"></i>&nbsp;Layout<br /> <i
+												class="fa fa-chevron-right"></i>&nbsp;Design
+										</p>
 									</div>
 									<div class="box-bottom">
 										<a href="#">Learn more</a>
 									</div>
 								</div>
 							</div>
-							<div class="col-lg-3">
+							<div class="col-lg-4">
 								<div class="box">
-									<div class="box-gray aligncenter" style="padding-bottom: 2px;">
+									<div class="box-gray aligncenter" style="padding-bottom: 19px;">
 										<h4>CONTROLS &#38; AUTOMATION</h4>
 										<div class="icon">
-										<i class="fa fa-edit fa-3x"></i>
+											<i class="fa fa-gears fa-3x"></i>
 										</div>
 										<p class="list_style">
-											<i class="fa fa-chevron-right"></i>&nbsp;Process<br/>
-											<i class="fa fa-chevron-right"></i>&nbsp;Controls Design<br/>
-											<span class="specail_list"><i class="fa fa-chevron-right"></i>&nbsp;Integration of Robotic &#38; Controls Systems</span><br/>
-											<i class="fa fa-chevron-right"></i>&nbsp;Installation<br/>
-											<i class="fa fa-chevron-right"></i>&nbsp;PLC Programming
-										</p>									
+											<i class="fa fa-chevron-right"></i>&nbsp;Process<br /> <i
+												class="fa fa-chevron-right"></i>&nbsp;Controls Design<br />
+											<span class="specail_list"><i class="fa fa-chevron-right"></i>&nbsp;Integration
+												of Robotic &#38; Controls Systems</span><br /> <i
+												class="fa fa-chevron-right"></i>&nbsp;Installation<br /> <i
+												class="fa fa-chevron-right"></i>&nbsp;PLC Programming
+										</p>
 									</div>
 									<div class="box-bottom">
 										<a href="#">Learn more</a>
 									</div>
 								</div>
 							</div>
-							<div class="col-lg-3">
+							<div class="col-lg-4">
 								<div class="box">
 									<div class="box-gray aligncenter" style="padding-bottom: 0px;">
 										<h4>MANUFACTURING &#38; SITE MANAGEMENT</h4>
 										<div class="icon">
-										<i class="fa fa-code fa-3x"></i>
+											<i class="fa fa-location-arrow fa-3x"></i>
 										</div>
 										<p class="list_style">
-											<i class="fa fa-chevron-right"></i>&nbsp;Manufacturing<br/>
-											<i class="fa fa-chevron-right"></i>&nbsp;Assembly<br/>
-											<i class="fa fa-chevron-right"></i>&nbsp;Installation<br/>
-											<i class="fa fa-chevron-right"></i>&nbsp;Tryouts<br/>
-											<i class="fa fa-chevron-right"></i>&nbsp;Buy Off
+											<i class="fa fa-chevron-right"></i>&nbsp;Manufacturing<br />
+											<i class="fa fa-chevron-right"></i>&nbsp;Assembly<br /> <i
+												class="fa fa-chevron-right"></i>&nbsp;Installation<br /> <i
+												class="fa fa-chevron-right"></i>&nbsp;Tryouts<br /> <i
+												class="fa fa-chevron-right"></i>&nbsp;Buy Off
 										</p>
-											
+
 									</div>
 									<div class="box-bottom">
 										<a href="#">Learn more</a>
 									</div>
 								</div>
 							</div>
-							<div class="col-lg-1">
-							</div>
-							
+							<div class="col-lg-1"></div>
+
 
 						</div>
 					</div>
@@ -156,65 +142,72 @@
 				<!-- divider -->
 				<div class="row">
 					<div class="col-lg-12">
-						<div class="solidline">
-						</div>
+						<div class="solidline"></div>
 					</div>
 				</div>
 				<!-- end divider -->
 				<!-- Portfolio Projects -->
-				<div class="row" style="display:none;">
+				<div class="row" style="display: none;">
 					<div class="col-lg-12">
 						<h4 class="heading">Recent Works</h4>
 						<div class="row">
 							<section id="projects">
-							<ul id="thumbs" class="portfolio">
-								<!-- Item Project and Filter Name -->
-								<li class="col-lg-3 design" data-id="id-0" data-type="web">
-								<div class="item-thumbs">
-								<!-- Fancybox - Gallery Enabled - Title - Full Image -->
-								<a class="hover-wrap fancybox" data-fancybox-group="gallery" title="Work 1" href="img/works/1.jpg">
-								<span class="overlay-img"></span>
-								<span class="overlay-img-thumb font-icon-plus"></span>
-								</a>
-								<!-- Thumb Image and Description -->
-								<img src="img/works/1.jpg" alt="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis elementum odio. Curabitur pellentesque, dolor vel pharetra mollis.">
-								</div>
-								</li>
-								<!-- End Item Project -->
-								<!-- Item Project and Filter Name -->
-								<li class="item-thumbs col-lg-3 design" data-id="id-1" data-type="icon">
-								<!-- Fancybox - Gallery Enabled - Title - Full Image -->
-								<a class="hover-wrap fancybox" data-fancybox-group="gallery" title="Work 2" href="img/works/2.jpg">
-								<span class="overlay-img"></span>
-								<span class="overlay-img-thumb font-icon-plus"></span>
-								</a>
-								<!-- Thumb Image and Description -->
-								<img src="img/works/2.jpg" alt="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis elementum odio. Curabitur pellentesque, dolor vel pharetra mollis.">
-								</li>
-								<!-- End Item Project -->
-								<!-- Item Project and Filter Name -->
-								<li class="item-thumbs col-lg-3 photography" data-id="id-2" data-type="illustrator">
-								<!-- Fancybox - Gallery Enabled - Title - Full Image -->
-								<a class="hover-wrap fancybox" data-fancybox-group="gallery" title="Work 3" href="img/works/3.jpg">
-								<span class="overlay-img"></span>
-								<span class="overlay-img-thumb font-icon-plus"></span>
-								</a>
-								<!-- Thumb Image and Description -->
-								<img src="img/works/3.jpg" alt="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis elementum odio. Curabitur pellentesque, dolor vel pharetra mollis.">
-								</li>
-								<!-- End Item Project -->
-								<!-- Item Project and Filter Name -->
-								<li class="item-thumbs col-lg-3 photography" data-id="id-2" data-type="illustrator">
-								<!-- Fancybox - Gallery Enabled - Title - Full Image -->
-								<a class="hover-wrap fancybox" data-fancybox-group="gallery" title="Work 4" href="img/works/4.jpg">
-								<span class="overlay-img"></span>
-								<span class="overlay-img-thumb font-icon-plus"></span>
-								</a>
-								<!-- Thumb Image and Description -->
-								<img src="img/works/4.jpg" alt="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis elementum odio. Curabitur pellentesque, dolor vel pharetra mollis.">
-								</li>
-								<!-- End Item Project -->
-							</ul>
+								<ul id="thumbs" class="portfolio">
+									<!-- Item Project and Filter Name -->
+									<li class="col-lg-3 design" data-id="id-0" data-type="web">
+										<div class="item-thumbs">
+											<!-- Fancybox - Gallery Enabled - Title - Full Image -->
+											<a class="hover-wrap fancybox" data-fancybox-group="gallery"
+												title="Work 1" href="img/works/1.jpg"> <span
+												class="overlay-img"></span> <span
+												class="overlay-img-thumb font-icon-plus"></span>
+											</a>
+											<!-- Thumb Image and Description -->
+											<img src="img/works/1.jpg"
+												alt="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis elementum odio. Curabitur pellentesque, dolor vel pharetra mollis.">
+										</div>
+									</li>
+									<!-- End Item Project -->
+									<!-- Item Project and Filter Name -->
+									<li class="item-thumbs col-lg-3 design" data-id="id-1"
+										data-type="icon">
+										<!-- Fancybox - Gallery Enabled - Title - Full Image --> <a
+										class="hover-wrap fancybox" data-fancybox-group="gallery"
+										title="Work 2" href="img/works/2.jpg"> <span
+											class="overlay-img"></span> <span
+											class="overlay-img-thumb font-icon-plus"></span>
+									</a> <!-- Thumb Image and Description --> <img
+										src="img/works/2.jpg"
+										alt="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis elementum odio. Curabitur pellentesque, dolor vel pharetra mollis.">
+									</li>
+									<!-- End Item Project -->
+									<!-- Item Project and Filter Name -->
+									<li class="item-thumbs col-lg-3 photography" data-id="id-2"
+										data-type="illustrator">
+										<!-- Fancybox - Gallery Enabled - Title - Full Image --> <a
+										class="hover-wrap fancybox" data-fancybox-group="gallery"
+										title="Work 3" href="img/works/3.jpg"> <span
+											class="overlay-img"></span> <span
+											class="overlay-img-thumb font-icon-plus"></span>
+									</a> <!-- Thumb Image and Description --> <img
+										src="img/works/3.jpg"
+										alt="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis elementum odio. Curabitur pellentesque, dolor vel pharetra mollis.">
+									</li>
+									<!-- End Item Project -->
+									<!-- Item Project and Filter Name -->
+									<li class="item-thumbs col-lg-3 photography" data-id="id-2"
+										data-type="illustrator">
+										<!-- Fancybox - Gallery Enabled - Title - Full Image --> <a
+										class="hover-wrap fancybox" data-fancybox-group="gallery"
+										title="Work 4" href="img/works/4.jpg"> <span
+											class="overlay-img"></span> <span
+											class="overlay-img-thumb font-icon-plus"></span>
+									</a> <!-- Thumb Image and Description --> <img
+										src="img/works/4.jpg"
+										alt="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis elementum odio. Curabitur pellentesque, dolor vel pharetra mollis.">
+									</li>
+									<!-- End Item Project -->
+								</ul>
 							</section>
 						</div>
 					</div>
@@ -222,14 +215,14 @@
 
 			</div>
 		</section>
-	<!-- end container -->
+		<!-- end container -->
 
-	<!-- start footer -->
+		<!-- start footer -->
         <?php include("footer.html");?>
 	<!-- end footer -->
-</div>
-<a href="#" class="scrollup"><i class="fa fa-angle-up active"></i></a>
-<!-- start common footer -->
+	</div>
+	<a href="#" class="scrollup"><i class="fa fa-angle-up active"></i></a>
+	<!-- start common footer -->
         <?php include("includes/common_footer.html");?>
 <!-- end common footer -->
 </body>
